75876172c7173cff434ca4fdbfabc9ae4331c996
Innextinit <localhost@innext.com>
2021-06-24 14:41:10 -0400

    want to write frontend, and have let go of using openjpg

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const home = require("../controller/index.controller")
 
 router.use("/users", require("./users-route"))
 router.use("/password", require("./password-route"))
+//router.use("/keys", require("../routes/openpgp-key"))
 router.use("/", home.welcome)
 
 module.exports = router
